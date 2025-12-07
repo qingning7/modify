d498e2badd111979252b07c6a99912e4ce7b60ae
qingning7 <18806119666@163.com>
2025-12-07 16:22:51 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-        base: '/modify/',
+        base: '/Babyhan/',
       server: {
         port: 3000,
         host: '0.0.0.0',
